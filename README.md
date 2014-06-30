fdb4e65af7c3c55f10270a576734c24108e912fe
LakshmiManjunath <lakshmi.cs87@gmail.com>
2014-06-30 15:16:14 -0400

    Project1 commit

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1>Project1:About me</h1>
+<p><b>Live URL:</b></p>
+<p><b>Description of the Project:</b> This project is a single page website,
+ that gives brief introduction about my previous qualifications and experience in Web-Development!!</p>
+<p><b>Outside Source:</b> I have made use of Google-Fonts</p>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>About Me</title>
+	<link href="css/about-me.css" type="text/css" rel="stylesheet">
+	<link href='http://fonts.googleapis.com/css?family=Amaranth' rel='stylesheet' type='text/css'>
+</head>
+<body>
+	<h1>Me..Myself<img id="me_myself" src="images/me_myself.jpg" alt="me_myself"> &amp; Who..<img id="who" src="images/who2.jpg" alt="who"></h1>
+	<img src="images/mypics.jpg" alt="my-pic" >
+	<div>
+		<p id="intro"><b>NAME:</b>Lakshmi Manjunath<br><b>EMAIL</b>:lakshmimanjunath@g.harvard.edu|<b>ADDRESS:</b>Brighton,MA</p>
+		<h3>1. In 2+ sentences describe who you are and what you do.</h3>
+		<p>I am originally from Bengaluru,India. I moved to Boston 2 years ago.I have a bachelor's degree in Computer Science with more than a year 
+		experience working as a Software Engineer. I have recently started building 
+		my career in Web Development and building my skill set in the same</p>
+		<h3>2. Why are you taking this course?</h3>
+		<p>I am planning to do <b><a href="http://www.extension.harvard.edu/degrees-programs/information-technology">Masters in Information technology</a></b> at <b><a href="http://www.extension.harvard.edu/">Harvard School of Extension</a></b>. And I have signed up for this course to fulfil the admission requirements towards the same</p>
+		<h3>3. What previous experience do you have with HTML/CSS?</h3>
+		<p> I had couple of Web related courses during my Bachelor's Degree, have completed few course-projects using html,css and php. Also I have finished <b><a href="http://www.extension.harvard.edu/courses/fundamentals-website-development">CSCIE-12</a></b> from Harvard School of Extension(Jan-May2014)</p>
+		<h3>4. What previous experience do you have with programming?</h3>
+		<p>I have extensively used Java and Cobol during my work-period. C,C++ and mySQL are some of the other languages I am familiar with.</p>
+		<h3>5. Have you worked with PHP before? If so, to what extent?</h3>
+		<p>I have used PHP for course-works and projects during my school days and also in <b><a href="http://www.extension.harvard.edu/courses/fundamentals-website-development">CSCIE-12</a></b></p>
+		<h3>6. Are you working on a Mac, PC, or some other platform?</h3>
+		<p>I currently use <b>PC</b>, but have worked on MAC and Ubuntu as well</p>
+		<h3>7. Anything else we should know?</h3>
+		<p>I am currently preping myself to take-up <b>Free-Lance Projects!!</b></p>
+	</div>
+</body>
+</html>
